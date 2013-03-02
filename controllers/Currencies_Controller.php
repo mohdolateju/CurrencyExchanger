b643295407b3f38e344225f88d87111ea401e24d
Huma Qaiser <huma@wellogic.com>
2013-03-02 11:20:08 +0400

    removing tool reference

--- controllers/Currencies_Controller.php
+++ controllers/Currencies_Controller.php
@@ -1,10 +1,5 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: Mohammed
- * Date: 28/02/13
- * Time: 22:00
- * To change this template use File | Settings | File Templates.
  */
 class Currencies_Controller
 {
