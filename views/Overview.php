6cc854ee94224d12bb0a1dc506fd00e6f3d99f0e
mohdolateju <mohdolateju@gmail.com>
2013-03-02 10:59:14 +0400

    Overview Page updated

--- views/Overview.php
+++ views/Overview.php
@@ -5,19 +5,6 @@
         <title>Overview :: Currency Exchanger</title>
         <link href="../css/main.css" rel="stylesheet" type="text/css"/>
         <link rel="icon" href="../images/money.png" type="image/x-icon"/>
-        <script type="text/javascript">			
-			function removeAtt(no){				
-				var tags=document.getElementsByTagName('input');				
-					if(no==1){
-						tags[0].removeAttribute('class');
-						tags[0].value='';
-					}												
-					if(no==2){
-						tags[1].removeAttribute('class');
-						tags[1].value='';
-					}												
-			}						
-        </script>
     </head>
     <body>
         <div id="wrapper">
@@ -47,9 +34,10 @@
                    	  </center>
                   	</div>                  
                     <strong>Description</strong>
-                    	<p>The currency exchanger application is a network application that calculates an amount money from one currency to another, its basic technologies are HTML, CSS, Javascript, PHP and MYSQL. The currency exchanger also uses AJAX, XML and Web Services to get Currency Rates, List of Countries, IP Address of the user browsing the system and also the registered users information from the database. It was created by Mohammed, Huma and Umar.</p>
+                    	<p>The currency exchanger application is a network application that calculates an amount money from one currency to another, its basic technologies are HTML 4, CSS 3, Javascript 1.8.5, PHP 5.3.0 and MYSQL 5. The currency exchanger also uses AJAX, XML and REST Web Services to get Currency Rates, List of Countries, IP Address of the user browsing the system and also the registered users information from the database. It was created by Mohammed, Huma and Umar. </p>
+                    	<p><strong>Note:</strong> Internet explorer might not render the pages properly, for best results view with recent versions of Google Chrome, Mozilla Firefox or Apple Safari.</p>
                     <strong>Mohammed.B.Olateju (H00139396)</strong>
-                    	<p>Mohammed is responsible for the currency exchange and the user section. The currency exchanger section requires the user of the currency web service to get the currency rates and display them, the user can also search for currency rates. The user section is where users can register to keep a history of the currencies that have been exchanged, the user will be able to view past transaction history on login.</p> 
+               	  <p>Mohammed is responsible for the currency exchange and the user section. The currency exchanger section requires the user of the currency web service to get the currency rates and display them, the user can also search for currency rates. The user section is where users can register to keep a history of the currencies that have been exchanged, the user will be able to view past transaction history on login.</p> 
                     <strong>Huma Qaiser (H00145644 )</strong>
                     	<p>Huma's section is the currency exchange calculator which is the home page of the application. The currency exchange calculator  uses the currency rates gotten from the currency exchange section to calculate an amount from on currency to another. It will also makes use of an IP address web service to get the current location of the user and provide the currency on the form automatically.</p>
                     <strong>Umar Jidda (H00)</strong>
