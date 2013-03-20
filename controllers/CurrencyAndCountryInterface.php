706fd9d82f2792c70214b6ef8c34710894361f84
mohdolateju <mohdolateju@gmail.com>
2013-03-21 00:09:37 +0400

    Currency Exchange Web Serviced Added

---
+++ controllers/CurrencyAndCountryInterface.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Mohammed
+ * Date: 20/03/13
+ * Time: 18:52
+ * To change this template use File | Settings | File Templates.
+ */
+interface CurrencyAndCountry
+{
+     function getCountriesList();
+     function getCurrenciesList();
+}
