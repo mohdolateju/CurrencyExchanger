0129d200a55af55c5c970ce7144d8f123d703e12
Mohammed <mohdolateju@gmail.com>
2013-05-05 23:52:32 +0400

    Currency Page Done

--- views/Currencies.php
+++ views/Currencies.php
@@ -6,6 +6,22 @@
         <link href="../css/main.css" rel="stylesheet" type="text/css"/>
         <link rel="icon" href="../images/money.png" type="image/x-icon"/>
         <script type="text/javascript" src="../javascript/main.js"></script>
+        <script type="text/javascript" src="../javascript/jquery-1.9.1.js"></script>
+        <script type="text/javascript">
+            function getRateCurrencyByAbbrev(){
+
+                //get country and currency values from the fields
+                var country=$("#country").val();
+                var currency=$("#currency").val();
+
+                //send ajax query with "currencyAbbrev" as the arguement value
+                $.get("../controllers/Currencyrates_Controller.php?action=currencyAbbrev", {country: country},
+                    function(value){
+                        //get the value from the ajax request and print it in the currency abbrev text field
+                        $("#currency").val(value);
+                    });
+            }
+        </script>
     </head>
     <body>
         <div id="wrapper">
@@ -33,78 +49,76 @@
                    	  <center>Search for Currency</center>
                   	</div>                  
            		  	<form name="getCurrencies" action="#" method="post">
-                          <input type="text" class="initval" name="country" onclick="removeAtt(1)" 
-                          							style="margin-left:11%;" value="Search By Country"/>
-                          <input type="text" class="initval" name="currency" onclick="removeAtt(2)" 
-                          							style="margin-left:5%;" value="Search Currency"/>
-                          <input type="text" disabled="disabled" name="rate" style="margin-left:5%; width:45px;" value="USD"/>		
+                          <input type="text" id="country" class="initval" name="country" onclick="removeAtt(1)"
+                                 onkeydown="getRateCurrencyByAbbrev()"
+                          							style="margin-left:27%;" value="Search By Country"/>
+                          <input type="text" id="currency" disabled="disabled" name="rate" style="margin-left:5%;
+                                        width:45px;" value="USD"/>
                       <br/>
                     </form>
-                    <table width="100%" border="0" cellspacing="2" cellpadding="5">
+                    <?php
+                    //get Currency rate Controller file
+                    include("../controllers/Currencyrates_Controller.php");
+
+                    //get Currency rate Controller class
+                    $instance=new Currencyrates_Controller();
+
+                    // Getting the List Of Some Currencies to be Displayed from a web service
+                    $aed=$instance->searchCurrencyByName("United Arab Emirates")->getAbbrev();
+                    $gbp=$instance->searchCurrencyByName("United Kingdom")->getAbbrev();
+                    $ger=$instance->searchCurrencyByName("Germany")->getAbbrev();
+                    $fra=$instance->searchCurrencyByName("France")->getAbbrev();
+                    $jap=$instance->searchCurrencyByName("Japan")->getAbbrev();
+                    $chn=$instance->searchCurrencyByName("China")->getAbbrev();
+                    $rss=$instance->searchCurrencyByName("Russia")->getAbbrev();
+                    $pkn=$instance->searchCurrencyByName("Pakistan")->getAbbrev();
+                    $ngn=$instance->searchCurrencyByName("Nigeria")->getAbbrev();
+                    $ind=$instance->searchCurrencyByName("India")->getAbbrev();
+                    ?>
+                    <table width="100%" border="0" cellspacing="2" cellpadding="5" style="margin-left: 12%";>
                         <tr>
                             <td><strong>COUNTRY</strong></td>
-                            <td><strong>CURRENCY</strong></td>
-                            <td><strong>ABREV.</strong></td>
+                            <td><strong>CURRENCY ABREV.</strong></td>
                         </tr>
                         <tr>
                         	<td>United Arab Emirates</td>
-                            <td>Arab Emirate Dirhams</td>
-                            <td>AED</td>
+                            <td><?php echo $aed ?></td>
                         </tr>
                         <tr>
                         	<td>United Kingdom</td>
-                            <td>Pound</td>
-                            <td>BP</td>
-                        </tr>
-                        <tr>
-                        	<td>United States of America</td>
-                            <td>Dollar</td>
-                            <td>USD</td>
+                            <td><?php echo $gbp ?></td>
                         </tr>
                         <tr>
                         	<td>Germany</td>
-                            <td>Euro</td>
-                            <td>EUR</td>
+                            <td><?php echo $ger ?></td>
                         </tr>
                         <tr>
                         	<td>France</td>
-                            <td>Euro</td>
-                            <td>EUR</td>
-                        </tr>
-                        <tr>
-                        	<td>Kingdom of Saudia Arabia</td>
-                            <td>Arabian Riyal</td>
-                            <td>RYL</td>
+                            <td><?php echo $fra ?></td>
                         </tr>
                         <tr>
                         	<td>Japan</td>
-                            <td>Janapese Yen</td>
-                            <td>YEN</td>
+                            <td><?php echo $jap ?></td>
                         </tr>
                         <tr>
                         	<td>China</td>
-                            <td>Yuan</td>
-                            <td>YEN</td>
+                            <td><?php echo $chn ?></td>
                         </tr>
                         <tr>
                         	<td>Russia</td>
-                            <td>Ruble</td>
-                            <td>RBL</td>
+                            <td><?php echo $rss ?></td>
                         </tr>
                         <tr>
                         	<td>Pakistan</td>
-                            <td>Rupee</td>
-                            <td>PKR</td>
+                            <td><?php echo $pkn ?></td>
                         </tr>
                         <tr>
                         	<td>Nigeria</td>
-                            <td>Naira</td>
-                            <td>NGN</td>
+                            <td><?php echo $ngn ?></td>
                         </tr>
                         <tr>
                         	<td>India</td>
-                            <td>Rupee</td>
-                            <td>INR</td>
+                            <td><?php echo $ind ?></td>
                         </tr>                                        
                     </table>
                     <p></p>
